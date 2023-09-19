ef9aa2c9e8e26a23c6e9d33907b99a8560493f33
Aleksandra <abaibik@gmail.com>
2023-09-19 16:54:11 +0200

    add prev and next buttons

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { volumes } from "@/lib/data";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -10,6 +11,9 @@ export default function VolumeDetail() {
 
   const volume = volumes.find((volume) => volume.slug === slug);
   const { title, description, cover, books } = volume;
+
+  const nextVolume = volumes[volumes.indexOf(volume) + 1];
+  const prevVolume = volumes[volumes.indexOf(volume) - 1];
 
   return (
     <>
@@ -26,13 +30,19 @@ export default function VolumeDetail() {
           </li>
         ))}
       </ul>
-
       <Image
         src={cover}
         alt={`Cover image of ${title}`}
         width={140}
         height={230}
       />
+      {prevVolume ? (
+        <Link href={`/volumes/${prevVolume.slug}`}>Previous volume</Link>
+      ) : null}
+      <br />
+      {nextVolume ? (
+        <Link href={`/volumes/${nextVolume.slug}`}>Next volume</Link>
+      ) : null}
     </>
   );
 }
